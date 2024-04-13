40cb30324109aaf19ed8084bfbcb3bd71ee78894
Diego Argueta <dargueta1879@gmail.com>
2024-04-13 17:30:56 -0600

    added persistence

--- App.tsx
+++ App.tsx
@@ -16,20 +16,22 @@ import { rneuiTheme as theme } from './src/constants/'
 
 import Navigation from './src/navigation/drawer/screenNavigation';
 
-import store from './src/app/store'
-import { Provider } from 'react-redux'
+import { persistor, store } from './src/app/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 
 function App(): React.JSX.Element {
 
 
   return (
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
       <Navigation/>
       </ThemeProvider>
     </SafeAreaProvider>
-
+      </PersistGate>
   </Provider>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
-import configurationReducer from '../features/configurations/configurationSlide'
-export default configureStore({
-  reducer: {
-    configuration:configurationReducer
-  }
+import { configureStore} from "@reduxjs/toolkit";
+import configReducer from "../features/configurations/configurationSlide";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { persistReducer, persistStore,persistCombineReducers } from 'redux-persist';
+import thunk from 'redux-thunk';
+
+
+const rootReducer = { 
+  configuration:configReducer
+ }
+
+const persistConfig = {
+  key: 'root',
+  storage:AsyncStorage,
+}
+
+const persistedReducer = persistCombineReducers(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: thunk
 })
+
+export const persistor = persistStore(store)
