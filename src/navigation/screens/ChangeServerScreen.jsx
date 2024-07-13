2d5b1ef96f070ef0d8e7c2a35735ab1807808018
Diego Argueta <dargueta1879@gmail.com>
2024-07-13 11:16:54 -0600

    hot fix change url

--- src/navigation/screens/ChangeServerScreen.jsx
+++ src/navigation/screens/ChangeServerScreen.jsx
@@ -1,31 +1,37 @@
-import { StyleSheet, View,Alert } from 'react-native'
-import React from 'react'
+import { StyleSheet, View, Alert } from 'react-native'
+import React, { useState } from 'react'
 import { Container, FormInput, Text } from '../../components/common'
 import Theme from '../../constants/Theme'
 import { Button } from '@rneui/themed'
 import { useDispatch, useSelector } from 'react-redux'
-import { updateURL } from '../../features/configurations/configurationSlice'
+import { logOut, updateURL } from '../../features/configurations/configurationSlice'
 import { useNavigation } from '@react-navigation/native'
 
 const ChangeServerScreen = () => {
-const navigation  =  useNavigation();
-    const [url, seturl] = React.useState("");
-    const URL = useSelector(state => state.configuration.URL)
-   const dispatch= useDispatch();
-    const changeUrl=()=>{
-if(url!="" && url!=undefined){
-  dispatch(updateURL(url))
-navigation.navigate("ConfigurationScreen");
+  const [isLoading, setisLoading] = useState(false)
+  const navigation = useNavigation();
+  const [url, seturl] = React.useState("");
+  const URL = useSelector(state => state.configuration.URL)
+  const dispatch = useDispatch();
+  const changeUrl = () => {
+    setisLoading(true)
+    if (url != "" && url != undefined) {
+      dispatch(updateURL(`${url}/Api`))
+      setTimeout(() => {
+        setisLoading(false)
+        dispatch(logOut())
+      }, 1500);
 
-}else{
-  Alert.alert("Servidor inválido.")
-}
+
+    } else {
+      Alert.alert("Servidor inválido.")
     }
+  }
   return (
     <Container>
-     <FormInput onBlur={()=>{}} onFocus={()=>{}}  color={Theme.colors.primary} title={'URL: '} type='text' placeholder={'https://example.com'} val={url} onChange={seturl} />
-   <Text>{URL}</Text>
-    <Button onPress={changeUrl} title={'Guardar'}/>
+      <FormInput onBlur={() => { }} onFocus={() => { }} color={Theme.colors.primary} title={'URL: '} type='text' placeholder={'https://example.com'} val={url} onChange={seturl} />
+      <Text>{URL}</Text>
+      <Button loading={isLoading} onPress={changeUrl} title={'Guardar'} />
     </Container>
   )
 }
--- src/navigation/screens/LoginScreen.tsx
+++ src/navigation/screens/LoginScreen.tsx
@@ -1,129 +1,138 @@
-import { Image, PermissionsAndroid, Platform, StyleSheet, View} from 'react-native'
+import { Image, PermissionsAndroid, Platform, StyleSheet, View } from 'react-native'
 import React, { FC } from 'react'
 
 import Box from '../../components/common/Box'
 import NumericKeyword from '../../components/common/NumericKeyword';
 import { materialTheme } from '../../constants';
-import { login as doLogin, updateBluetoothPermission, updateToken} from '../../features/configurations/configurationSlice'
+import { login as doLogin, updateBluetoothPermission, updateToken } from '../../features/configurations/configurationSlice'
 import { useDispatch, useSelector } from 'react-redux';
 import { Alert } from '../../components/common';
 import axios from 'axios'
 import axiosClient from '../../utils/axiosClient';
 import { IRootState } from '../../app/store';
+import { Text } from '@rneui/themed';
 
 
-const LoginScreen:FC = () => {
-  const [valueText, setValueText] = React.useState("");
-  const [errormessage, seterrormessage] = React.useState("")
-  const [isLoading, setIsLoading] = React.useState(false)
-  const dispatch = useDispatch();
-const url = useSelector((state:IRootState)=>state.configuration.URL)
-  React.useEffect(() => {
-    requestCameraPermission();
-}, [])
+const LoginScreen: FC = () => {
+    const [valueText, setValueText] = React.useState("");
+    const [errormessage, seterrormessage] = React.useState("")
+    const [isLoading, setIsLoading] = React.useState(false)
+    const dispatch = useDispatch();
+    const url = useSelector((state: IRootState) => state.configuration.URL)
+    const token = useSelector((state: IRootState) => state.configuration.token)
+    React.useEffect(() => {
+        requestCameraPermission();
+    }, [])
 
 
-const requestCameraPermission = async () => {
-    console.log(Platform.Version)
+    const requestCameraPermission = async () => {
+        console.log(Platform.Version)
 
-    if (Number(Platform.Version) > 30) {
-        try {
-            const granted = await PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
-                {
-                    title: 'SC Restaurant',
-                    message:
-                        'Esta app Necesita acceder a tu bluetooth. ',
-                    buttonNeutral: 'Preguntar luego',
-                    buttonNegative: 'Cancel',
-                    buttonPositive: 'OK',
-                },
-            );
-            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-                console.log('You can use the bluetooth');
-                dispatch(updateBluetoothPermission("yes"))
-            } else {
-                console.log('Bluetooth permission denied');
+        if (Number(Platform.Version) > 30) {
+            try {
+                const granted = await PermissionsAndroid.request(
+                    PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+                    {
+                        title: 'SC Restaurant',
+                        message:
+                            'Esta app Necesita acceder a tu bluetooth. ',
+                        buttonNeutral: 'Preguntar luego',
+                        buttonNegative: 'Cancel',
+                        buttonPositive: 'OK',
+                    },
+                );
+                if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                    console.log('You can use the bluetooth');
+                    dispatch(updateBluetoothPermission("yes"))
+                } else {
+                    console.log('Bluetooth permission denied');
+                }
+            } catch (err) {
+                console.warn(err);
             }
-        } catch (err) {
-            console.warn(err);
+        } else {
+            dispatch(updateBluetoothPermission("yes"))
         }
-    } else {
-        dispatch(updateBluetoothPermission("yes"))
-    }
-};
+    };
 
-const login= async ()=>{
+    const login = async () => {
 
-console.log('url',url)
-try {
-    const credentials = {
-        username: 'Administrador',
-        password: 'Admin2024$',
-        grant_type: 'password'
-    }
-    setIsLoading(true);
-    axios({
-        method: 'post',
-        url: url + '/GetToken',
-        headers: { 'content-type': 'application/x-www-form-urlencoded' },
-        data: credentials
-    }).then(data=>{
-        
-       dispatch(updateToken({data:data.data.access_token,expiration_date:data.data.expiration_date}))
-    
-        axiosClient.post('/ObtenerUsuarioPorCodigo',{Codigo:valueText}).then((data2)=>{
-          
-           dispatch(doLogin({name:data2.data.Nombre,roomDefaultId:data2.data.SalonDefaultID,userId:data2.data.UsuarioID}))
-           setIsLoading(false);
-        }).catch(err=>{
+        console.log('url', url)
+        try {
+            const credentials = {
+                username: 'Administrador',
+                password: 'Admin2024$',
+                grant_type: 'password'
+            }
+            setIsLoading(true);
+            axios({
+                method: 'post',
+                url: url + '/GetToken',
+                headers: { 'content-type': 'application/x-www-form-urlencoded' },
+                data: credentials
+            }).then(data => {
+                dispatch(updateToken({ data: data.data.access_token, expiration_date: data.data.expiration_date }))
+
+                axios({
+                    method: 'post',
+                    url: url + '/ObtenerUsuarioPorCodigo',
+                    headers: { 'content-type': 'application/x-www-form-urlencoded', 'Authorization': `Bearer ${data.data.access_token}` },
+                    data: { Codigo: valueText }
+                }).then(data2 => {
+                    dispatch(doLogin({ name: data2.data.Nombre, roomDefaultId: data2.data.SalonDefaultID, userId: data2.data.UsuarioID }))
+                    setIsLoading(false);
+
+                }).catch(err => {
+                    setIsLoading(false);
+                    seterrormessage('Hubo un error. Intentelo más tarde por favor.')
+                })
+
+            }).catch(error => {
+                setIsLoading(false);
+                seterrormessage('Hubo un error en la conexión. Revise su conexión a internet.')
+            })
+        } catch (error) {
             setIsLoading(false);
-            seterrormessage('Credenciales inválidas.')
-        })
-    }).catch(error=>{
-        setIsLoading(false);
-        seterrormessage('Hubo un error en la conexión. Revise su conexión a internet.')
-    }) 
-} catch (error) {
-    setIsLoading(false);
-    seterrormessage('Error inesperado, pruebas más tarde.')
-}
+            seterrormessage('Error inesperado, pruebas más tarde.')
+        }
 
- // dispatch(doLogin())
-}
+        // dispatch(doLogin())
+    }
 
 
- 
-  return (
-    <View style={styles.container}>
-          <Alert setMessage={seterrormessage} type={'warning'}  message={errormessage} />
-       <View style={styles.logoContainer}><Image resizeMode='contain' style={styles.logo} source={require('../../assets/img/logo.png')}/></View>
-    <Box center>
-    <NumericKeyword isLoading={isLoading} login={login} valueText={valueText} setValueText={setValueText}/>
-   
-    </Box>
-    </View>
-  )
+
+    return (
+        <View style={styles.container}>
+            <Alert setMessage={seterrormessage} type={'warning'} message={errormessage} />
+            <View style={styles.logoContainer}><Image resizeMode='contain' style={styles.logo} source={require('../../assets/img/logo.png')} /></View>
+            <Box center>
+                <NumericKeyword isLoading={isLoading} login={login} valueText={valueText} setValueText={setValueText} />
+                <Text>{url}</Text>
+            </Box>
+        </View>
+    )
 }
 
 export default LoginScreen
 
 const styles = StyleSheet.create({
-    container:{ flex:1,
-        flexDirection:'column',alignItems:'center',
-    backgroundColor:'#fff'},
-        
-    logoContainer:{
-        backgroundColor:materialTheme.colors.back_ground_color,
-        width:'100%',
-        justifyContent:'center',
-        alignItems:'center',
-        height:226
-       
+    container: {
+        flex: 1,
+        flexDirection: 'column', alignItems: 'center',
+        backgroundColor: '#fff'
     },
-    logo:{
-       
-        height:210
+
+    logoContainer: {
+        backgroundColor: materialTheme.colors.back_ground_color,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 226
+
+    },
+    logo: {
+
+        height: 210
     }
 
 })
