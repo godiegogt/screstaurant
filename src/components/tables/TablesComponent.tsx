a2abc4e45880da38a42dd5b47914212427d842bb
Diego Argueta <dargueta1879@gmail.com>
2024-08-03 13:42:31 -0600

    added save talbes in memory

--- src/components/tables/TablesComponent.tsx
+++ src/components/tables/TablesComponent.tsx
@@ -1,15 +1,17 @@
-import React, { useCallback,  useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { View, StyleSheet, ActivityIndicator, ScrollView, RefreshControl } from 'react-native'
 import { Text } from '../common'
 import TableItem from './TableItem'
 import { Card } from '@rneui/themed'
 import PageNavigator from '../common/PageNavigator'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { IRootState } from '../../app/store'
 import { useFocusEffect } from '@react-navigation/native';
 import Theme from '../../constants/Theme'
 import useRooms from '../../hooks/useRooms'
 import { IRoom } from '../../features/configurations/interfaces/IRoom'
+import roomsService from '../../services/RoomsService'
+import { updateRooms, updateTableStatus } from '../../features/configurations/configurationSlice'
 export type TableType = {
   MesaID: number,
   Nombre: string,
@@ -22,7 +24,98 @@ export type TableType = {
 const TablesComponent = () => {
   const roomDefault = useSelector((state: IRootState) => state.configuration.userData.roomDefaultId);
   const [roomSelected, setRoomSelected] = useState(roomDefault);
-  const roomsHook=useRooms();
+  const rooms = useSelector((state: IRootState) => state.configuration.rooms);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const dispatch = useDispatch()
+  const _getRooms = async () => {
+    let roomsData = null;
+    try {
+      setIsLoading(true);
+      roomsData = await roomsService.getRooms();
+
+      //Fill out rooms and tables
+      for (let index = 0; index < roomsData.length; index++) {
+        //Do request for each room to fill out its tables
+        roomsData[index].tables = await roomsService.getTables(roomsData[index].SalonID);
+      }
+
+      return roomsData
+
+    } catch (err) {
+      console.log(err)
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError('An unexpected error occurred');
+      }
+    } finally {
+      setIsLoading(false);
+      return roomsData;
+    }
+
+
+  }
+
+  // const getTables = async (SalondID: number) => {
+  //     try {
+  //         setIsLoading(true);
+  //         const tablesData = await roomsService.getTables(SalondID);
+  //         // dispatch(updateTables(tablesData))
+  //     } catch (err) {
+  //         if (err instanceof Error) {
+  //             setError(err.message);
+  //         } else {
+  //             setError('An unexpected error occurred');
+  //         }
+  //     } finally {
+  //         setIsLoading(false);
+  //     }
+
+
+  // }
+
+  const getTablesStatus = async (SalondID: number, newRooms: IRoom[]) => {
+
+    try {
+      setIsLoading(true);
+      const tableStatusData = await roomsService.getTablesStatus(SalondID);
+
+      if (tableStatusData.length > 0) {
+        const room = newRooms.find(room => room.SalonID == SalondID)
+        const tablesWithStatus = room?.tables.map((oldStatus) => {
+          for (let index = 0; index < tableStatusData.length; index++) {
+            if (oldStatus.MesaID == tableStatusData[index].MesaID) {
+              return { ...oldStatus, OrdenID: tableStatusData[index].OrdenID }
+            }
+          }
+
+          return oldStatus
+        });
+
+        if (tablesWithStatus) {
+          const newrooms = newRooms.map((salon) =>
+            salon.SalonID === SalondID
+              ? { ...salon, tables: tablesWithStatus }
+              : salon
+          );
+          dispatch(updateRooms(newrooms))
+        }
+      }
+
+    } catch (err) {
+      console.log('status error', err)
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError('An unexpected error occurred');
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+
 
   useFocusEffect(
     React.useCallback(() => {
@@ -32,17 +125,14 @@ const TablesComponent = () => {
   const getRooms = async () => {
     try {
       //Verify if there are not rooms
-      if(roomsHook.rooms.length == 0){
-        await roomsHook.getRooms();
-      }
 
-      await roomsHook.getTablesStatus(roomSelected)
+      const newRooms: any = await _getRooms();
+      await getTablesStatus(roomSelected, newRooms)
 
-   } catch (error) {
-    
-   }
+    } catch (error) {
+      setIsLoading(false)
+    }
   }
-
 
   const onRefresh = useCallback(() => {
     getRooms();
@@ -50,28 +140,28 @@ const TablesComponent = () => {
 
 
   return (
-   <ScrollView
-   refreshControl={
-    <RefreshControl refreshing={roomsHook.isLoading} onRefresh={onRefresh} />
-  }>
-     <Card>
-      <Card.Title><Text h4 bold>Mesas</Text></Card.Title>
-      <Card.Divider />
-      {roomsHook.rooms != undefined && roomsHook.rooms.length > 0 && <PageNavigator rooms={roomsHook.rooms} roomSelected={roomSelected} setRoomSelected={setRoomSelected} />}
-      {
-        !roomsHook.isLoading
-        ?
-        <View style={styles.container}>
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={isLoading} onRefresh={onRefresh} />
+      }>
+      <Card>
+        <Card.Title><Text h4 bold>Mesas</Text></Card.Title>
+        <Card.Divider />
+        {rooms != undefined && rooms.length > 0 && <PageNavigator rooms={rooms} roomSelected={roomSelected} setRoomSelected={setRoomSelected} />}
         {
-          roomsHook.rooms.find((room:IRoom)=>room.SalonID==roomSelected) != undefined && roomsHook.rooms.find((room:IRoom)=>room.SalonID==roomSelected)?.tables.map((table:TableType) => <TableItem key={table.MesaID} item={table} />)
+          !isLoading
+            ?
+            <View style={styles.container}>
+              {
+                rooms.find((room: IRoom) => room.SalonID == roomSelected) != undefined && rooms.find((room: IRoom) => room.SalonID == roomSelected)?.tables.map((table: TableType) => <TableItem key={table.MesaID} item={table} />)
+              }
+            </View>
+            :
+            <ActivityIndicator color={Theme.colors.primary} />
         }
-      </View>
-      :
-      <ActivityIndicator color={Theme.colors.primary}/>
-      }
 
-    </Card>
-   </ScrollView>
+      </Card>
+    </ScrollView>
   )
 
 }
---
+++ src/interfaces/IRoom.ts
@@ -0,0 +1,7 @@
+import { TableType } from "../components/tables/TablesComponent";
+
+export interface IRoom{
+    Name:string,
+    SalonID:number,
+    tables :Array<TableType>
+}
